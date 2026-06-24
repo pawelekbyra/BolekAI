776c21ba4b6604e36b17ec959e9c7d85638fae6a
pawelekbyra <pawelek.byra@gmail.com>
2026-06-24 11:01:34 +0200

    refactor: use permission engine for tool approval list

--- lib/approval.ts
+++ lib/approval.ts
@@ -1,1 +1,6 @@
-export { TOOLS_REQUIRING_APPROVAL } from '@/lib/agent/tool-registry';
+import { requiresApproval } from './agent/permission-engine';
+import { toolRegistry } from './agent/tool-registry';
+
+export const TOOLS_REQUIRING_APPROVAL = Object.values(toolRegistry)
+  .filter((tool) => requiresApproval(tool.risk))
+  .map((tool) => tool.name);
